713f1d843f826880de977916c26f1b72b6f29871
Olie Auger <olie.auger@gmail.com>
2022-01-27 10:38:07 -0500

    Add color

--- noise/noise.js
+++ noise/noise.js
@@ -16,16 +16,21 @@ function draw() {
     let now = Date.now();
     for (let i = 0; i < canvas.width; i += 10) {
         for (let j = 0; j < canvas.height; j += 10) {
-            let v = noise.get(now / 2000, j / 100, i / 100);
-            let angle = v * Math.PI * 2;
-            let x = Math.cos(angle) * 15;
-            let y = Math.sin(angle) * 15;
+            let ki = i;//Math.log2(i ** 50) * Math.log(Math.sqrt(i * i + j * j));
+            let kj = j;//Math.log2(j ** 50) * Math.log(Math.sqrt(i * i + j * j));
+            let v = noise.get(now / 2000, kj / 500, ki / 500);
+            let angle = v * Math.PI * 16;
+            let x = Math.cos(angle) * 10;
+            let y = Math.sin(angle) * 10;
             ctx.lineCap = 'round';
             ctx.lineWidth = 10;
             ctx.beginPath();
             ctx.moveTo(i, j);
             ctx.lineTo(i + x, j + y);
-            ctx.strokeStyle = '#fff4';
+            // ctx.strokeStyle = '#fff4';
+            let cv = Math.abs(v - noise.get(now / 2000 + 10, (kj) / 500, (ki) / 500)) + Math.abs(v - noise.get(now / 2000 - 10, (kj) / 500, (ki) / 500));
+            cv **= 2;
+            ctx.strokeStyle = `hsla(${v * 360}, 100%, 50%, ${cv})`;
             ctx.stroke();
         }
     }
